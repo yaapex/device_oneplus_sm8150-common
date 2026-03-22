2dcc921015a2f3337ea29251d1b16f195aeafe84
xenxynon <valkyrie.xps@gmail.com>
2026-03-22 19:19:59 +0530

    Revert "sm8150-common: begin building with Lindroid"
    This reverts commit f11bb742577eea0159c081c5225f252e28de268c.

--- common.mk
+++ common.mk
@@ -356,9 +356,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     KProfiles
 
-# Lindroid
-TARGET_BUILD_LINDROID := true
-
 # Media
 PRODUCT_COPY_FILES += \
     hardware/qcom-caf/sm8150/media/conf_files/msmnile/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
